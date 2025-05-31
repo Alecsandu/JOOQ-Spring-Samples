f394264f575f96080e44248f857477948de7e898
Alexandru-Cristian Sandu <alecsandu678@gmail.com>
2025-05-31 15:59:54 +0300

    Update test

--- src/test/java/com/dockerino/demo/DemoApplicationTests.java
+++ src/test/java/com/dockerino/demo/DemoApplicationTests.java
@@ -1,13 +1,17 @@
 package com.dockerino.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void load() {
+
 	}
 
 }
