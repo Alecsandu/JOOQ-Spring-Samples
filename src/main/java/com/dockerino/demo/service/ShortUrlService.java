01528ddadc51d80fe65185d356b32553d4f17fef
Alexandru-Cristian Sandu <alecsandu678@gmail.com>
2025-10-03 22:24:20 +0300

    [Refactor] Make login return only jwt token
    For userinfo the client has to use the userinfo endpoint
    
    Signed-off-by: Alexandru-Cristian Sandu <alecsandu678@gmail.com>

--- src/main/java/com/dockerino/demo/service/ShortUrlService.java
+++ src/main/java/com/dockerino/demo/service/ShortUrlService.java
@@ -17,7 +17,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
