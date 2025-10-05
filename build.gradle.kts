eb250ee7bfc34134e7392739433aafcb24a48204
Alexandru-Cristian Sandu <alecsandu678@gmail.com>
2025-10-05 22:23:34 +0300

    [Dependency] Add new dependency spring-security-oauth2-jose for jwt decoding
    More details at https://docs.spring.io/spring-security/reference/servlet/oauth2/resource-server/jwt.html
    
    Signed-off-by: Alexandru-Cristian Sandu <alecsandu678@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ val jooqVersion = "3.19.26"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.security:spring-security-oauth2-jose")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-jooq")
